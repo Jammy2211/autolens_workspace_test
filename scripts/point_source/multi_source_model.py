a6bdce15cf08ec560713029d2726f16d44d4db0c
Richard <richard@rghsoftware.co.uk>
2024-06-17 16:51:18 +0100

    script illustrating how to do multi source at the autofit level

--- scripts/point_source/multi_source_model.py
+++ scripts/point_source/multi_source_model.py
@@ -18,36 +18,41 @@ lens_galaxy = al.Galaxy(
     mass=isothermal_mass_profile,
 )
 
-centre = af.TuplePrior(
-    centre_0=af.UniformPrior(lower_limit=-0.1, upper_limit=0.1),
-    centre_1=af.UniformPrior(lower_limit=-0.1, upper_limit=0.1),
-)
-
-point_source = af.Model(
-    al.ps.PointSourceChi,
-    centre=centre,
-)
-
-model = af.Collection(
-    source=point_source,
-    lens=lens_galaxy,
-)
-
-
-analysis = AnalysisAllToAllPointSource(
-    coordinates=[
-        (-1.29, -0.98),
-        (1.02, 1.11),
-        (0.43, -1.58),
-        (-1.61, 0.27),
+analyses = []
+multiple_image_collections = [
+    [
+        (1.4103868896870786, 0.6578124999999888),
+        (-0.6094369426810129, -1.4421875000000037),
+        (1.0346581598160152, -1.1992187500000047),
+        (-1.2161057802279271, 1.138281249999987),
     ],
-    error=0.1,
-    grid=grid,
+    [
+        (-0.9341964691001788, 1.3906249999999865),
+        (1.5673539941230086, -0.23437500000000808),
+        (-0.6680740793955845, -1.376562500000004),
+        (1.5835919704439665, 0.05937499999999091),
+    ],
+]
+
+analysis = sum(
+    AnalysisAllToAllPointSource(
+        coordinates=coordinates,
+        error=0.1,
+        grid=grid,
+    ).with_model(
+        af.Collection(
+            lens=lens_galaxy,
+            source=af.Model(
+                al.ps.Point,
+            ),
+        )
+    )
+    for coordinates in multiple_image_collections
 )
 
 if __name__ == "__main__":
     search = af.DynestyStatic("point_source")
 
-    result = search.fit(model=model, analysis=analysis)
+    result = search.fit(model=None, analysis=analysis)
 
     print(result)
